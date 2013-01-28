8bfbd1025196d6650d04f6a65133c1cc64fee9f6
Jim Rosser <jarosser06@gmail.com>
2013-01-28 13:24:30 -0600

    added named_conf_options as configurable

---
+++ manifests/options.pp
@@ -0,0 +1,15 @@
+class dns::options (
+  $options      => '',
+  $forward_ips  => '',
+) {
+  
+  file { "/etc/bind/named.conf.options":
+    owner   => root,
+    group   => bind,
+    mode    => 644,
+    require => Class['dns::server::config'],
+    source  => template("dns/named_conf_options.erb"),
+    notify  => Class['dns::server::service'],
+  }
+
+}
