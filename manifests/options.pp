66f6f31837fe30b3b38f091d1f936a17a382446a
Jim Rosser <jarosser06@gmail.com>
2013-01-28 15:30:36 -0600

    changed source to content

--- manifests/options.pp
+++ manifests/options.pp
@@ -9,7 +9,7 @@ class dns::options (
     group   => bind,
     mode    => 644,
     require => Class['dns::server::config'],
-    source  => template('dns/named_conf_options.erb'),
+    content => template('dns/named_conf_options.erb'),
     notify  => Class['dns::server::service'],
   }
 
